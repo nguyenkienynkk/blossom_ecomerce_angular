48c5a91f52235b52bfc47f32f0b95dbbb743a16d
nguyenkien <nguyenkienynk@gmail.com>
2024-05-16 11:13:58 +0700

    #11 - dashboard admin

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserResponse } from 'src/app/responses/user/user.response';
+import { TokenService } from 'src/app/service/token.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
-
+  userResponse?: UserResponse | null;
+  constructor(
+    private userService: UserService,
+    private tokenService: TokenService,
+    private router: Router,
+  ) {
+  }
+  ngOnInit() {
+    this.userResponse = this.userService.getUserResponseToLocalStorage();
+    // Default router
+    debugger
+    if (this.router.url === '/admin') {
+      this.router.navigate(['/admin/orders']);
+    }
+  }
+  logout() {
+    this.userService.removeUserFromLocalStorage();
+    this.tokenService.removeToken();
+    this.userResponse = this.userService.getUserResponseToLocalStorage();
+    this.router.navigate(['/']);
+  }
+  showAdminComponent(componentName: string): void {
+    if (componentName === 'orders') {
+      this.router.navigate(['/admin/orders']);
+    } else if (componentName === 'categories') {
+      this.router.navigate(['/admin/categories']);
+    } else if (componentName === 'products') {
+      this.router.navigate(['/admin/products']);
+    }
+  }
 }
